469de679588d2518156507dbea4fb96ebdb5dbbf
PROLOVE <proLove0212@gmail.com>
2022-12-27 18:17:15 +0700

    add register button at <Hero/> and add additional Countdown section before Intro

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,8 @@ function App() {
   return (
     <main className="bg-slate-100 relative overflow-hidden">
       <Hero />
-      <div className="max-w-7xl mx-auto px-4 md:px-12 lg:px-20 pb-20 space-y-20">
+      <div className="max-w-7xl mx-auto px-4 md:px-12 lg:px-20 pb-20 space-y-20 mt-20">
+        <Countdown />
         <Intro />
         <Departments />
         <Timeline />
